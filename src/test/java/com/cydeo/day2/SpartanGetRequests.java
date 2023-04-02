6c3ecdcb1263073f6a7328919d0ed8d707389ecb
Bilge Ergün <blgergn@gmail.com>
2023-04-02 17:49:36 +0300

    first test case is done

--- src/test/java/com/cydeo/day2/SpartanGetRequests.java
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -1,8 +1,14 @@
 package com.cydeo.day2;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SpartanGetRequests {
 
-    String url = "http://18.234.192.234:8000/api/spartans";
+    String url = "http://18.234.192.234:8000";
 
 
    //    Given Accept type application/json
@@ -12,7 +18,33 @@ public class SpartanGetRequests {
    //    And response body should include spartan result
 
 
+      @Test
+      public void test1(){
 
+          Response response = RestAssured.given().accept(ContentType.JSON)
+                  .when()
+                  .get(url + "/api/spartans");
+
+          //printing status code from response object
+          System.out.println("response.statusCode() = " + response.statusCode());
+
+          //printing response content type from response object
+          System.out.println("response.contentType() = " + response.contentType());
+
+          //print whole result body
+
+          response.prettyPrint();
+
+          //how to do API testing then ?
+
+          //verify status code is 200
+          Assertions.assertEquals(response.statusCode(),200);
+
+          //verify content type is application/json
+          Assertions.assertEquals(response.contentType(),"application/json");
+
+
+      }
 
 
 
