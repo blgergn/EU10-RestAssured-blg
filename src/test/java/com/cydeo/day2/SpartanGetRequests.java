e18bfab73f539c26b21ef2c455dae4806f224685
Bilge Ergün <blgergn@gmail.com>
2023-04-02 17:16:26 +0300

    day02 first testcase

---
+++ src/test/java/com/cydeo/day2/SpartanGetRequests.java
@@ -0,0 +1,20 @@
+package com.cydeo.day2;
+
+public class SpartanGetRequests {
+
+    String url = "http://18.234.192.234:8000/api/spartans";
+
+
+   //    Given Accept type application/json
+   //    When user send GET request to api/spartans end point
+   //    Then status code must 200
+   //    And response Content Type must be application/json
+   //    And response body should include spartan result
+
+
+
+
+
+
+
+}
